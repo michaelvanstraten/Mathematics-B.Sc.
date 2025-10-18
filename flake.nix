9889ad512eb5495db8e3abeb2d56e941b16c3d0e
Michael van Straten <michael@vanstraten.de>
2025-10-18 14:05:01 +0200

    feat(flake.nix): Add a dev shell with ghdl environment

--- flake.nix
+++ flake.nix
@@ -68,6 +68,49 @@
                 pkgs.texlivePackages.latexindent
               ];
             };
+
+            devShells.ds-so-se-2025 =
+              let
+                llvmPackages = pkgs.llvmPackages_latest;
+                stdenv = llvmPackages.stdenv;
+                mkShell = pkgs.mkShell.override { inherit stdenv; };
+
+                ghdl-llvm = stdenv.mkDerivation rec {
+                  pname = "ghdl-llvm";
+                  version = "5.1.1";
+
+                  src = pkgs.fetchurl {
+                    url = "https://github.com/ghdl/ghdl/releases/download/v${version}/ghdl-llvm-${version}-macos15-aarch64.tar.gz";
+                    sha256 = "sha256-BEVlKkYNAauU06lfqI85iulVCXPa9Yc/ZsJIDSxz4gk=";
+                  };
+
+                  installPhase = ''
+                    ls
+                    mkdir -p $out
+                    cp -r * $out/
+                  '';
+
+                  meta = {
+                    homepage = "https://github.com/ghdl/ghdl";
+                    platforms = [ "aarch64-darwin" ];
+                  };
+                };
+
+                riscv32 = import nixpkgs {
+                  inherit system;
+                  crossSystem = {
+                    config = "riscv32-none-elf";
+                  };
+                };
+
+              in
+              mkShell {
+                packages = [
+                  ghdl-llvm
+                  pkgs.zlib
+                  riscv32.buildPackages.gcc
+                ];
+              };
           }
         )
       );
