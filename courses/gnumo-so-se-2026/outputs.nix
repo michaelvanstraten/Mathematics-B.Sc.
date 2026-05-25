c99cafaa85341acde4d0a108b0f040a92aca81c0
Michael van Straten <michael@vanstraten.de>
2026-05-25 12:26:43 +0200

    gnumo: add missing typst packages to nix drv

--- courses/gnumo-so-se-2026/outputs.nix
+++ courses/gnumo-so-se-2026/outputs.nix
@@ -17,6 +17,21 @@ flake-utils.lib.eachDefaultSystem (
               version = "0.6.1";
               hash = "sha256-aBYoozFFknJcZvT3ZC3YQXmZchEPNiMeEp/YrS51+Qo=";
             }
+            {
+              name = "cetz";
+              version = "0.4.2";
+              hash = "sha256-qBIEHqtiMSG/WoXHPC/rQ9VkestSvVNlUwTmAMX1wAs=";
+            }
+            {
+              name = "oxifmt";
+              version = "1.0.0";
+              hash = "sha256-edTDK5F2xFYWypGpR0dWxwM7IiBd8hKGQ0KArkbpHvI=";
+            }
+            {
+              name = "cetz-plot";
+              version = "0.1.3";
+              hash = "sha256-JtZJh8xIGgntdDQj8YBDlYLF6/QQXOt9tLaRYTtgyso=";
+            }
           ];
         }
         // {
